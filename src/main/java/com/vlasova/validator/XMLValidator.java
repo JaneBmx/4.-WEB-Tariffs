a7640d7ba724a1c2793e6d5ec77de4a08c3aa074
Jane Sonina <janeridebmx@gmail.com>
2020-03-23 17:51:19 +0300

    Remove vulnerability

--- src/main/java/com/vlasova/validator/XMLValidator.java
+++ src/main/java/com/vlasova/validator/XMLValidator.java
@@ -2,7 +2,6 @@ package com.vlasova.validator;
 
 //import org.apache.logging.log4j.LogManager;
 //import org.apache.logging.log4j.Logger;
-import com.vlasova.exception.ValidateXMLException;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
